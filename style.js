7281bae6ec23ea3da09e8286778092712784b1d6
Giovanna Furlan Torres <giovanna.torres@sou.inteli.edu.br>
2022-04-27 19:53:34 -0300

    Update Menu Hamburguer

---
+++ style.js
@@ -0,0 +1,10 @@
+const btn = document.getElementById('btn')
+
+function menu(event){
+    if (event.type == 'touchstart') event.preventDefault()
+    const btn = document.getElementById('nav');
+    nav.classList.toggle('active');
+}
+
+btn.addEventListener('click', menu);
+btn.addEventListener('touchstart', menu);
